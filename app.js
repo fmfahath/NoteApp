92938e61316b7e6887c54544dcdeb0c5e5e2571f
fmfahath <fmfahath.dev@gmail.com>
2023-11-20 02:58:16 +0530

    .container/.notes-containe ; js => localstorage save function  added

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const createBtn = document.getElementsByClassName("btn");
 const notesContainer = document.getElementsByClassName("notes-container");
-let notes = document.getElementsByClassName("input-box");
+let notes = document.querySelectorAll(".input-box");
+
+showData();
 
 
-// ----------------------------------------
+function saveData(){
+    localStorage.setItem("data", notesContainer[0].innerHTML);
+}
 
+function showData(){
+    notesContainer[0].innerHTML = localStorage.getItem("data");
+}
 
-
-// -----------------------------------------
 createBtn[0].addEventListener('click', ()=>{
     // console.log("hi");
 
@@ -19,11 +24,21 @@ createBtn[0].addEventListener('click', ()=>{
     imgEle.src = "img/delete.png";
 
     notesContainer[0].appendChild(inputBoxEle).appendChild(imgEle);
+
+    saveData();
 });
 
 notesContainer[0].addEventListener('click', (e)=>{
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove();
+        saveData();
+    }
+    else if(e.target.tagName === "P"){
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                saveData();
+            }
+        })
     }
 });
 
