6052b7840193dabdd904382812ae016fa0f3cd61
fmfahath <fmfahath.dev@gmail.com>
2023-11-20 02:35:44 +0530

    .container/.notes-container : js => notes delete function added

--- app.js
+++ app.js
@@ -14,12 +14,20 @@ createBtn[0].addEventListener('click', ()=>{
     let inputBoxEle = document.createElement('p');
     let imgEle = document.createElement('img');
 
-    inputBoxEle.setAttribute('contenteditable', 'true');
+    inputBoxEle.setAttribute('contentEditable', 'true');
     inputBoxEle.className = 'input-box';
     imgEle.src = "img/delete.png";
 
     notesContainer[0].appendChild(inputBoxEle).appendChild(imgEle);
-})
+});
+
+notesContainer[0].addEventListener('click', (e)=>{
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+    }
+});
+
+
 
 
 
